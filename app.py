123122308816cb040cf57aaea62ea676121b8663
emma5004 <izuemmmnuel500@gmail.com>
2026-08-06 22:06:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def home():
 def browse():
     return render_template('browse.html')
 
+@app.route('/find-skills')
+def find_skills():
+    return render_template('find-skills.html')
+
 @app.route('/teach')
 def teach():
     return render_template('teach.html')
@@ -18,10 +22,6 @@ def teach():
 @app.route('/teach-skill')
 def teach_skill():
     return render_template('teach-skill.html')
-
-@app.route('/find-skills')
-def find_skills():
-    return render_template('find-skills.html')
 
 @app.route('/login')
 def login():
@@ -32,5 +32,5 @@ def signup():
     return render_template('signup.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)

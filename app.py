a0af1b16400f94955d089489cb97fb7a50c924ac
emma5004 <izuemmmnuel500@gmail.com>
2026-08-06 23:19:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
@@ -7,21 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/browse')
-def browse():
-    return render_template('browse.html')
-
 @app.route('/find-skills')
 def find_skills():
     return render_template('find-skills.html')
 
-@app.route('/teach')
-def teach():
-    return render_template('teach.html')
-
 @app.route('/teach-skill')
 def teach_skill():
     return render_template('teach-skill.html')
+
+@app.route('/browse')
+def browse():
+    return render_template('browse.html')
 
 @app.route('/login')
 def login():
@@ -32,5 +27,4 @@ def signup():
     return render_template('signup.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run()
